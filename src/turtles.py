c600ebf1add82e7b183f1f8aa8c5309062041da0
Roman Mussi <Roman Mussi@RM-Notebook>
2019-05-11 11:37:22 -0300

    Cambio nombre de modulo, era igual a lib turtle y no funcionaba import

---
+++ src/turtles.py
@@ -0,0 +1,17 @@
+'''
+Created on 9 may. 2019
+
+@author: Roman Mussi
+'''
+
+import turtle
+bob = turtle.Turtle()
+print(bob)
+
+for i in range(4):
+    bob.fd(100)
+    bob.lt(90)
+
+turtle.mainloop()
+
+
