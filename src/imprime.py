b72b4d48bcb76b7be43b0b2f765d1486c49ffa22
Román <Romn@192.168.56.1>
2019-06-12 15:35:24 -0300

    Agrego modulo

---
+++ src/imprime.py
@@ -0,0 +1,14 @@
+'''
+Created on 19 may. 2017
+
+@author: Román
+'''
+
+def imprime_vertical(texto):
+    for c in texto:
+        print(c)
+
+def imprime_horizontal(texto):
+    print(texto)
+
+#print(__name__)
