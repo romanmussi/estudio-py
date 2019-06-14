3de249d9937d428c020fd7dd5982c42bf905014e
romanmussi <romanmussi@gmail.com>
2019-06-14 19:39:20 -0300

    Limpio líneas de prueba git

--- src/prueba1.py
+++ src/prueba1.py
@@ -4,18 +4,10 @@ Created on 9 may. 2019
 @author: Roman Mussi
 '''
 
-#agregado desde pc estudio
-
 from imprime import imprime_vertical, imprime_horizontal
 
 _h = imprime_horizontal
 _v = imprime_vertical
-
-#agregado desde note para evaluar como impacta en pc estudio
-
-#otro agregado en note, misma linea que agregado en pc estudio
-
-#agregado desde pc estudio
 
 texto = "hola mundo"
 _h(texto)
