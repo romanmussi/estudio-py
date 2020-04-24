a6f1763049673a5de6fbec18759acdd7a6bf1dcc
romanmussi <romanmussi@gmail.com>
2020-04-24 19:46:01 -0300

    Practicas Think Py

--- src/turtles.py
+++ src/turtles.py
@@ -6,11 +6,20 @@ Created on 9 may. 2019
 
 import turtle
 bob = turtle.Turtle()
-print(bob)
+#print(bob)
 
-for i in range(4):
-    bob.fd(100)
-    bob.lt(90)
+def square(t, length):
+    for i in range(4):
+        t.fd(length)
+        t.lt(90)
+
+def polygon(t, length, n):
+    for i in range(n):
+        t.fd(length)
+        t.lt(360/n)
+
+#square(bob, 50)
+polygon(bob, 100, 9)
 
 turtle.mainloop()
 
